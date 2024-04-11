59726cfaf9a943f572069436a91ae97052e12142
Arthur <arthur@cloudawesome.uk>
2024-04-11 13:28:14 +0100

    Action configuration for Pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://cloud-awesome.github.io/efrs-sandbox/',
+  url: 'https://cloud-awesome.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/efrs-sandbox/',
